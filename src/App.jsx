7ddd3f116de63722669339f7c643e4d76c8fa041
Aman Yadav <149204784+diya5722@users.noreply.github.com>
2024-06-05 15:09:40 +0530

    Started Site

---
+++ src/App.jsx
@@ -0,0 +1,14 @@
+import { useState } from 'react'
+import './index.css'
+import Navbar from "./Components/Navbar";
+function App() {
+  const [count, setCount] = useState(0)
+
+  return (
+    <>
+      <Navbar />
+    </>
+  )
+}
+
+export default App
